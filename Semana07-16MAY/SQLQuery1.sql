d998d3b1e0d7160ac292a9caf688088165b7993c
Gustavo Coronel <gcoronelc@gmail.com>
2024-05-15 17:54:05 -0500

    Semana 7

--- Semana07-16MAY/SQLQuery1.sql
+++ Semana07-16MAY/SQLQuery1.sql
@@ -48,6 +48,103 @@ go
 
 
 
+select * from productos
+where ProductID=5000000;
+go
+
+
+
+alter table productos
+add constraint pk_productos
+primary key ( productid );
+go
+
+
+select * from productos
+where ProductID=5000000;
+go
+
+
+
+select *  
+from productos
+where CategoryID = 2 and UnitPrice <= 10;
+go
+
+
+select ProductID, ProductName 
+from productos
+where CategoryID = 2 and UnitPrice <= 10;
+go
+
+
+select * 
+from Products p
+join Categories c on p.CategoryID = c.CategoryID;
+go
+
+select * 
+into categorias
+from Categories;
+go
+
+
+select * 
+from productos p
+join categorias c on p.CategoryID = c.CategoryID;
+go
+
+
+
+-- Consultar el producto masvendido por categoria
+
+select *
+from (
+	select t.CategoryID, t.ProductID, t.cant,
+	RANK() OVER (PARTITION BY t.CategoryID ORDER BY t.cant DESC)
+		AS cant_rank
+	from (
+		select p.CategoryID, p.ProductID,  sum(d.Quantity) cant
+		from [Order Details] d
+		join Products p on d.ProductID = p.ProductID
+		group by p.CategoryID, p.ProductID
+	) t
+) x
+where x.cant_rank = 1;
+
+select p.*
+from Products p
+join ( 
+select *
+from (
+	select t.CategoryID, t.ProductID, t.cant,
+	RANK() OVER (PARTITION BY t.CategoryID ORDER BY t.cant DESC)
+		AS cant_rank
+	from (
+		select p.CategoryID, p.ProductID,  sum(d.Quantity) cant
+		from [Order Details] d
+		join Products p on d.ProductID = p.ProductID
+		group by p.CategoryID, p.ProductID
+	) t
+) x
+where x.cant_rank = 1 ) t
+on ( p.CategoryID=t.CategoryID and p.ProductID = t.ProductID );
+go
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
